b666730274a57666903d78a9b188f59586260330
Cindy Han <chan@colgate.edu>
2014-10-14 14:42:57 -0400

    wrote the user input part

--- main.c
+++ main.c
@@ -12,10 +12,29 @@
 #include <signal.h>
 
 
+
+
+
 int main(int argc, char **argv) {
-
-    // a blank slate :-)
-
+	char input[1024]; // array for the input
+    // prompt the user
+    do {
+    	printf("Type in a prompt: \n");
+    	fflush(stdout);
+    	fgets(input, 1024, stdin);
+    	// check if the user wants to quit
+    	if (strcmp(input, "exit") == 0){
+    		break;
+    	}
+    	// split the input into sections
+    	
+    	// make a child
+    	pid_t pid = fork();
+    	// have the child do the process
+    	
+    	
+    
+    }
     return 0;
 }
 
