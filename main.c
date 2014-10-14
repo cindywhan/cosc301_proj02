105278ea9e4f1057104080181ac3e3cc7b686c76
Cindy Han <chan@colgate.edu>
2014-10-14 16:37:26 -0400

    made a function to split the input, getting started on the execv part with the children

--- main.c
+++ main.c
@@ -12,7 +12,42 @@
 #include <signal.h>
 
 
+char **split_input(char *input){
+	// this functions spilts the user input for the execv function to use
+	// find out how long to make the return array
+	char *cp = strdup(input);
+	char *tok = strtok(cp, " \t\n");
+	int count = 1;
+	while (tok != NULL){
+		tok = strtok(NULL, " \t\n");
+		count++;
+	}
+	free(cp);
+	// make the return array
+	char **rv = malloc((count + 1) * sizeof(char *));
+	char *copy = strdup(input);
+	// split the input into sections
+	char *token = strtok(copy, " \t\n");
+	int index = 0;
+	while (token != NULL){
+		rv[index] = strdup(token);
+		token = strtok(NULL, " \t\n");
+		index++;
+	}
+	free(copy);
+	return rv;
+	
 
+}
+
+void free_tokens(char **arr){
+	int i = 0;
+	while (arr[i] != NULL){
+		free(arr[i]); // free each string
+		i++;
+	}
+	free(arr);
+}
 
 
 int main(int argc, char **argv) {
@@ -22,19 +57,22 @@ int main(int argc, char **argv) {
     	printf("Type in a prompt: \n");
     	fflush(stdout);
     	fgets(input, 1024, stdin);
+    	
     	// check if the user wants to quit
-    	if (strcmp(input, "exit") == 0){
+    	if (strcmp(input, "exit\n") == 0){
     		break;
     	}
     	// split the input into sections
-    	
+    	char **commands = split_input(input);
+    	int i = 0;
     	// make a child
-    	pid_t pid = fork();
+    	//pid_t pid = fork();
     	// have the child do the process
     	
-    	
+    	free_tokens(commands);
     
     }
+    while(1);
     return 0;
 }
 
