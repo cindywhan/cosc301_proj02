fa250d5ae6989dd717cb75abe1d25059ccd06f0c
Cindy Han <chan@colgate.edu>
2014-10-15 17:30:19 -0400

    it almost works

--- main.c
+++ main.c
@@ -11,16 +11,6 @@
 #include <poll.h>
 #include <signal.h>
 
-
-//modifies string to lowercase in place
-void to_lower(char (*str)[1024]){
-	for (int i = 0; i <= strlen(*str); i++){
-		if (*str[i]>=65 && *str[i]<=90){
-			*str[i] = *str[i]+32;
-		}
-	}
-}
-
 char **split_input(char *input, char *sub){
 	// this functions spilts the user input based on the semicolons in the file
 	// figure out how long to make the return array 
@@ -58,37 +48,18 @@ char **split_input(char *input, char *sub){
 void runseq(char **cmdarr){
 //this function will run through the array of commands, fork for each command, and call the execv() for each process
 //sequential mode
-
-  	int childstatus;
-  //go through command tokens  
-   for (int i = 0; i < sizeof(cmdarr)/sizeof(char*); i++){
-    	
-		//if the current command isn't the last one but is the exit command
-    	if (strcmp(cmdarr[i], "exit") && i!=sizeof(cmdarr)-1){
-    		continue;
-    	}
-    
-    	//fork each process as the for loop goes through array of commands, cmdarr
-    	pid_t child = fork();
-    	
-    	if (child < 0){
-    		printf ("Fork failed\n");
-    		break;
-    	}
-    	else if (child == 0){
-    		//C: is this necessary? mode p is not a command, it is just the user input, no?
-    		
-    		//else, run the command
-    		execv(cmdarr[i], cmdarr);
-    		
-    		//if execv runs properly, error statement below will not print
-    		printf("error");
-    	} 
-    	else{
-    		//need to change to waitpid at some point
-    		wait(&childstatus);
-    	}
+	// make a child
+    pid_t pid = fork();
+    if (pid < 0){
+    	printf ("Fork failed\n");
     }
+    else if (pid == 0){
+    	if (execv(cmdarr[0], cmdarr) < 0){
+    		fprintf(stderr, "execv failed\n"); // child executes the code
+    	}
+    } 
+    int child = 0;
+    wait(&child);
 }
 
 void runpar(char **cmdarr){
@@ -137,31 +108,35 @@ void free_tokens(char **arr){
 	free(arr);
 }
 
-void cut_comments(char (*input)[1024]){
+char *cut_comments(char *input){
 	// takes in the input and gets rid of any comments
-	int size = sizeof(*input) / sizeof(char);
-	for (int i = 0; i < size; i++){
-		if (*input[i] == '#'){
+	char *rv = malloc(sizeof(input));
+	
+	for (int i = 0; i < strlen(input) - 1; i++){
+		if (input[i] == '#'){
 		// anything after # is a comment
-			*input[i] = '\0';	
+			rv[i] = '\0';	
+		}
+		else{
+			rv[i] = tolower(input[i]);
 		}
 	}
+	return rv;
 }
 
 
 
 int main(int argc, char **argv) {
 	char input[1024]; // array for the input
-
+	memset(input, 0, 1024);
     // prompt the user
     while(1) {
     	printf("prompt>  ");
     	fflush(stdout);
     	fgets(input, 1024, stdin);
     	
-    	to_lower(&input);
     	// take out comments
-    	cut_comments(&input);
+    	char *in = cut_comments(input);
 		int mode = 0; // default mode (0) is seq and mode (1) is par
     	
     	char **commands = split_input(input, ";");
@@ -199,6 +174,7 @@ int main(int argc, char **argv) {
     		free_tokens(cmd);
     		}
     	free_tokens(commands);
+    	free(in);
     }
 }
 
