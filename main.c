b7fe7cd6e54be7b5dc8754d524649c5e4ce9ca8d
jcseo3 <jseo@colgate.edu>
2014-10-15 03:00:01 -0400

    adding job, process structs, and some forking stuff

--- main.c
+++ main.c
@@ -12,6 +12,32 @@
 #include <signal.h>
 
 
+//create a struct proc for processes
+struct proc{
+	struct proc *next;
+	pid_t pid;
+	char complete; //returns true if process is completed
+};
+
+//create a struct job
+struct job{
+	struct job *next;
+	process *first; //list of processes in the job
+};
+
+
+//this function will return true(1) once all processes in the job are completed
+int jobstatus (job *job){
+	process proc;
+	
+	for (proc=job->first; proc; proc=proc->next){
+		if(!proc->complete){
+			return 0;
+		}
+	return 1;
+	}
+}
+
 char **split_input(char *input){
 	// this functions spilts the user input for the execv function to use
 	// find out how long to make the return array
@@ -72,7 +98,30 @@ int main(int argc, char **argv) {
     	free_tokens(commands);
     
     }
-    while(1);
+    while(1){
+    	prinf ("$ ");
+    	if(fgets(input, 1024, stdin) == NULL){
+    		printf("error\n");
+    		break;
+    	}
+    	
+    	int child = fork();
+    	
+    	if (child<0){
+    		printf ("Fork failed\n");
+    		break;
+    	}
+    	
+    	else if (child == 0){
+    		if (strcmp(*commands, "cd") == 0){
+    		
+    		}
+    	}
+    	
+    	else{
+    		wait();
+    	}
+    }
     return 0;
 }
 
