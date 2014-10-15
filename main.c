c57029ea560054ba0a06b4dacff55f9073057587
Cindy Han <chan@colgate.edu>
2014-10-15 17:53:13 -0400

    ugh ugh

--- main.c
+++ main.c
@@ -108,11 +108,37 @@ void free_tokens(char **arr){
 	free(arr);
 }
 
+char **format_input(char **in){
+	// format the command for the execv function
+	// format = [command, options, NULL]
+	char **rv = malloc(3 * sizeof(char *));
+	char options[32];
+	memset(options, 0, 32);
+	rv[0] = strdup(in[0]);
+	int i = 1;
+	int index = 0;
+	// collect all the options for the command
+	while(in[i] != NULL){
+		if (in[i][0] == '-'){
+			for (int n = 1; n < strlen(in[i]); n++){
+				options[index] = in[i][n];
+				index++;
+			}
+		}
+	}
+	options[index] = '\0';
+	// copy options into the format return array
+	rv[1] = strdup(options);
+	rv[2] = NULL;
+	return rv;
+
+}
+
 char *cut_comments(char *input){
 	// takes in the input and gets rid of any comments
 	char *rv = malloc(sizeof(input));
 	
-	for (int i = 0; i < strlen(input) - 1; i++){
+	for (int i = 0; i < strlen(input); i++){
 		if (input[i] == '#'){
 		// anything after # is a comment
 			rv[i] = '\0';	
@@ -139,42 +165,44 @@ int main(int argc, char **argv) {
     	char *in = cut_comments(input);
 		int mode = 0; // default mode (0) is seq and mode (1) is par
     	
-    	char **commands = split_input(input, ";");
+    	char **commands = split_input(in, ";");
+    	free(in);
     	for (int i = 0; i < sizeof(commands)/sizeof(char *); i++){
     		// check if the command is valid
     		char **cmd = split_input(commands[i], " \t\n");
-    		if (strcmp(cmd[0],"mode") == 0 || strcmp(cmd[0],"sequential") ==0 || strcmp(cmd[0],"parallel") == 0){
+    		char **format = format_input(cmd);
+    		free_tokens(cmd);
+    		if (strcmp(format[0],"mode") == 0 || strcmp(format[0],"sequential") ==0 || strcmp(format[0],"parallel") == 0){
     			// change the mode 
-    			if (strcmp(cmd[0], "mode") == 0){
-    				if (strcmp(cmd[1], "s") == 0){
+    			if (strcmp(format[0], "mode") == 0){
+    				if (strcmp(format[1], "s") == 0){
     					mode = 0;
     				}
-    				else if (strcmp(cmd[1], "p") == 0){
+    				else if (strcmp(format[1], "p") == 0){
     					mode = 1;
     				}
     			}
-    			else if (strcmp(cmd[0], "sequential") == 0){
+    			else if (strcmp(format[0], "sequential") == 0){
     				mode = 0;
     			}
-    			else if (strcmp(cmd[0], "parallel") == 0){
+    			else if (strcmp(format[0], "parallel") == 0){
     				mode = 1;
     			}
     		} 
-    		else if (cmd[0] != NULL){
+    		else if (format[0] != NULL){
     			switch(mode){
     			case 1: // run parallel code
     				runpar(commands + i);
     				break;
     			default:
-    				runseq(cmd);
+    				runseq(format);
     				break;
     			}
     			
     		}
-    		free_tokens(cmd);
+    		free_tokens(format);
     		}
     	free_tokens(commands);
-    	free(in);
     }
 }
 
